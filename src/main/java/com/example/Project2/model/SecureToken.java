ebe8293414bd0dad85b472c41d61864a8d1beb0e
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2026-01-04 19:00:12 +0300

    aded secure token model

---
+++ src/main/java/com/example/Project2/model/SecureToken.java
@@ -0,0 +1,20 @@
+package com.example.Project2.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class SecureToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // or AUTO
+    private Long id;
+    private String token;
+    private LocalDateTime expireAt;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}

d6e0cbde42fc89890c98c41bc35c03aa7b035447
wesamAlsaleh <wesammuneer7@gmail.com>
2026-01-06 18:31:14 +0300

    feat: create service table

---
+++ src/main/java/com/example/Project2/model/Service.java
@@ -0,0 +1,32 @@
+package com.example.Project2.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "services")
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "service_id")
+    private Long serviceId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "service")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    // FK's:
+    @JoinColumn(name = "schedule_id")
+    private Long scheduleId;
+
+    @JoinColumn(name = "provider_id")
+    private Long providerId;
+}

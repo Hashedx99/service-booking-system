fa706cdf5689272c41b04044c298a3d65e282447
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2026-01-08 21:26:21 +0300

    added the seeding file , later on we can also add services , properties , profiles , and seed many things by putting them in the run method

---
+++ src/main/java/com/example/Project2/config/DataSeeder.java
@@ -0,0 +1,32 @@
+package com.example.Project2.config;
+
+import com.example.Project2.model.User;
+import com.example.Project2.model.UserProfile;
+import com.example.Project2.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataSeeder implements CommandLineRunner {
+   private final UserRepository userRepository;
+   public DataSeeder(UserRepository userRepository){
+       this.userRepository=userRepository;
+   }
+    @Override
+    public void run(String... args) throws Exception {
+            userSeed();
+    }
+    private void userSeed(){
+       User user = new User();
+       user.setPassword("$2a$10$/8Yh51sFJaukKOWbFES0IuwNB8q0M0XihNvR/1097DmJfb/kE9S0O");
+       user.setActivated(true);
+       user.setAccountVerified(true);
+       user.setUserName("meemo");
+       user.setEmailAddress("test@gmail.com");
+        UserProfile userProfile = new UserProfile();
+        userProfile.setFirstName("ali");
+        userProfile.setLastName("isa");
+       user.setUserProfile(userProfile);
+       userRepository.save(user);
+    }
+}

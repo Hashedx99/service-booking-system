b09252950704e4bcf10aced813e8ec8040fc73bb
Hamza <hshabx99@gmail.com>
2026-01-21 04:03:31 +0300

    feat: remove unused imports and query method from PropertyScheduleRepository

--- src/main/java/com/ga/project2/repository/PropertyScheduleRepository.java
+++ src/main/java/com/ga/project2/repository/PropertyScheduleRepository.java
@@ -1,14 +1,8 @@
 package com.ga.project2.repository;
 
 import com.ga.project2.model.PropertySchedule;
-import com.ga.project2.model.ServiceSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface PropertyScheduleRepository extends JpaRepository<PropertySchedule, Long> {
-    @Query("SELECT s FROM PropertySchedule s WHERE s.property.propertyId = :id")
-    List<PropertySchedule> findByPropertyId(@Param("id") Long propertyId);
+
 }

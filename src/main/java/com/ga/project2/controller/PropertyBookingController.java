8818bb2c05b46a9b4266331818870353415a64fb
Hamza <hshabx99@gmail.com>
2026-01-21 04:01:54 +0300

    feat: simplify PropertyBookingController methods and handle not found cases

--- src/main/java/com/ga/project2/controller/PropertyBookingController.java
+++ src/main/java/com/ga/project2/controller/PropertyBookingController.java
@@ -1,5 +1,6 @@
 package com.ga.project2.controller;
 
+import com.ga.project2.exception.InformationNotFoundException;
 import com.ga.project2.model.PropertyBooking;
 import com.ga.project2.service.PropertyBookingService;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +21,14 @@ public class PropertyBookingController {
 
     // Create booking
     @PostMapping
-    public ResponseEntity<PropertyBooking> createBooking(@RequestBody CreatePropertyBooking model) {
-        PropertyBooking booking = propertyBookingService.createBooking(model.getBookingDate(), model.getUserId(), model.getPropertyId());
-        return ResponseEntity.ok(booking);
+    public PropertyBooking createBooking(@RequestBody CreatePropertyBooking model) {
+        return propertyBookingService.createBooking(model.getBookingDate(), model.getUserId(), model.getPropertyId());
     }
 
     // Get booking by ID
     @GetMapping("/{id}")
-    public ResponseEntity<PropertyBooking> getBooking(@PathVariable Long id) {
-        return propertyBookingService.getBooking(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public PropertyBooking getBooking(@PathVariable Long id) {
+        return propertyBookingService.getBooking(id).orElseThrow(()-> new InformationNotFoundException("Booking with ID "+id+" not found"));
     }
 
     // Get booking by propertyID
@@ -47,16 +45,15 @@ public class PropertyBookingController {
 
     // Update booking date
     @PutMapping("/date")
-    public ResponseEntity<PropertyBooking> updateBookingDate(@RequestBody UpdateBookingDate model) {
-        PropertyBooking updated = propertyBookingService.updateBookingDate(model.getId(), model.getNewDate());
-        return ResponseEntity.ok(updated);
+    public PropertyBooking updateBookingDate(@RequestBody UpdateBookingDate model) {
+        return propertyBookingService.updateBookingDate(model.getId(), model.getNewDate());
+
     }
 
     // Soft delete booking
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> softDeleteBooking(@PathVariable Long id) {
+    public void softDeleteBooking(@PathVariable Long id) {
         propertyBookingService.softDeleteBooking(id);
-        return ResponseEntity.noContent().build();
     }
 
     // Get only active bookings
